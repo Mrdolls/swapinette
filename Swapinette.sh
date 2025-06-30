4e39cf72fa4d950f6ba0557b4ae7bb3ea997c7c3
Matthieu Gingast <mgingast@f6r13s4.clusters.42paris.fr>
2025-06-30 16:59:12 +0200

    auto push

---
+++ Swapinette.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# bash Swapinette [repetitions] [nombres de nombres generes aleatoirement pour push_swap] [limit a ne pas depasser]
+i=1
+total=$1
+size=$2
+max_moves=$3
+
+while [ $i -le $total ]; do
+    ARG="$(shuf -i 0-99 -n $size | tr '\n' ' ')"
+    INDEX="$(./push_swap $ARG | wc -l)"
+    if [ $INDEX -gt $max_moves ]; then
+        echo -e "\n\e[31mKO ➜ $INDEX operations (limite $max_moves)\e[0m"
+        exit 1
+    fi
+    percent=$(( i * 100 / total ))
+    bar=$(printf "%-${percent}s" "#" | tr ' ' '#')
+    printf "\rProgression : [%-100s] %d%%" "$bar" "$percent"
+
+    ((i++))
+done
+
+echo -e "\n\e[92mOK - Toutes les operations respectent la limite ($max_moves)\e[0m"

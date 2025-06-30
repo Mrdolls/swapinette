69e4871d2f3c4c357f1f111373c338c971439bb4
Matthieu Gingast <mgingast@f6r13s4.clusters.42paris.fr>
2025-06-30 17:31:21 +0200

    auto push

--- Swapinette.sh
+++ Swapinette.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
-# bash Swapinette [repetitions] [nombres de nombres generes aleatoirement pour push_swap] [limit a ne pas depasser]
-i=1
+
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <push_swap_exec> <nb_tests> <size_of_list> <max_moves>"
+    exit 1
+fi
+
+exec_name=$1
 total=$2
 size=$3
 max_moves=$4
+i=1
 
 while [ $i -le $total ]; do
-    ARG="$(shuf -i 0-$size -n $size | tr '\n' ' ')"
-    INDEX="$(./"$1" $ARG | wc -l)"
-    if [ $INDEX -gt $max_moves ]; then
+    ARG="$(shuf -i 0-$(($size - 1)) -n $size | tr '\n' ' ')"
+    INDEX="$(./"$exec_name" $ARG | wc -l)"
+
+    if [ "$INDEX" -gt "$max_moves" ]; then
         echo -e "\n\e[31mKO ➜ $INDEX operations (limite $max_moves)\e[0m"
         exit 1
     fi
+
     percent=$(( i * 100 / total ))
-    bar=$(printf "%-${percent}s" "#" | tr ' ' '#')
+    filled=$(( percent ))
+    bar=$(printf "%-${filled}s" "#" | tr ' ' '#')
     printf "\rProgression : [%-100s] %d%%" "$bar" "$percent"
 
     ((i++))

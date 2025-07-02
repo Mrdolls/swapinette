c1053faaf047aecb640c6b3471e6f3b9f3f29677
Matthieu Gingast <mgingast@f6r7s7.clusters.42paris.fr>
2025-07-02 13:29:41 +0200

    auto push

--- Swapinette.sh
+++ Swapinette.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-if [ "$#" -ne 4 ]; then
-    echo "Usage: $0 <executable> <nb_tests> <size_of_list> <max_operations>"
+if [ "$#" -ne 5 ]; then
+    echo "Usage: $0 <executable> <checker> <nb_tests> <size_of_list> <max_operations>"
     exit 1
 fi
 
 exec_name=$1
-total=$2
-size=$3
-max_moves=$4
-i=1
+checker=$2
+total=$3
+size=$4
+max_moves=$5
 
-while [ $i -le $total ]; do
+### Première boucle : vérification du nombre d'opérations
+echo "➤ Test 1 : Vérification du nombre d'opérations..."
+
+for ((i=1; i<=total; i++)); do
     ARG="$(shuf -i 0-$(($size - 1)) -n $size | tr '\n' ' ')"
     INDEX="$(./"$exec_name" $ARG | wc -l)"
 
     if [ "$INDEX" -gt "$max_moves" ]; then
-        echo -e "\n\e[31mKO ➜ $INDEX operations (limite $max_moves)\e[0m"
+        echo -e "\n\e[31mKO ➜ $INDEX opérations (limite $max_moves)\e[0m"
+        echo "Arguments : $ARG"
         exit 1
     fi
 
@@ -24,8 +28,28 @@ while [ $i -le $total ]; do
     filled=$(( percent ))
     bar=$(printf "%-${filled}s" "#" | tr ' ' '#')
     printf "\rProgression : [%-100s] %d%%" "$bar" "$percent"
-
-    ((i++))
 done
 
-echo -e "\n\e[92mOK - Toutes les operations respectent la limite ($max_moves)\e[0m"
+echo -e "\n\e[92m✔ Toutes les opérations sont sous la limite ($max_moves)\e[0m"
+
+### Deuxième boucle : vérification avec le checker choisi
+echo -e "\n➤ Test 2 : Vérification avec $checker..."
+
+for ((i=1; i<=total; i++)); do
+    ARG="$(shuf -i 0-$(($size - 1)) -n $size | tr '\n' ' ')"
+    RESULT=$(./"$exec_name" $ARG | ./"$checker" $ARG)
+
+    if [ "$RESULT" != "OK" ]; then
+        echo -e "\n\e[31mKO avec $checker ➜ Résultat: $RESULT\e[0m"
+        echo "Arguments : $ARG"
+        exit 1
+    fi
+
+    percent=$(( i * 100 / total ))
+    filled=$(( percent ))
+    bar=$(printf "%-${filled}s" "#" | tr ' ' '#')
+    printf "\rProgression : [%-100s] %d%%" "$bar" "$percent"
+done
+
+echo -e "\n\e[92m✔ Toutes les vérifications $checker sont OK\e[0m"
+

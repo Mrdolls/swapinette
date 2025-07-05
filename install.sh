1ed2f0d9c795c69feb6549bf3b749261c4d6b110
Mrdolls <101651928+Mrdolls@users.noreply.github.com>
2025-07-05 02:07:43 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REPO_URL="git@github.com:Mrdolls/swapinette.git"
+REPO_URL="REPO_URL="https://github.com/Mrdolls/swapinette.git"
 INSTALL_DIR="$HOME/.swapinette"
 COMMAND_NAME="swapinette"
 
